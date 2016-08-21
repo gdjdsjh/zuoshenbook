b119257307206837e92cef8b0be1a46519bc027b
sjh <gdjdsjh@126.com>
2016-08-21 18:13:29 +0800

    LeetCode344 反转一个字符串

---
+++ src/ReverseString_344.java
@@ -0,0 +1,19 @@
+/**
+ * Created by sjh on 2016/8/21.
+ * 反转一个字符串
+ */
+public class ReverseString_344 {
+    public String reverseString(String s) {
+        if(s == null || s.length() == 0 || s.length() == 1){
+            return s;
+        }
+        char[] strings = s.toCharArray();
+        int mid = s.length()/2;
+        for(int i = 0; i < mid; i++){
+            char tmp = strings[i];
+            strings[i] = strings[s.length()-1-i];
+            strings[s.length()-1-i] = tmp;
+        }
+        return new String(strings);
+    }
+}
